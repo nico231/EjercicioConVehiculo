7673f9bde5c94521f68db8958b5143c20e6b9296
Nicolas Arboleda <zickk231@gmail.com>
2012-09-03 16:59:01 -0500

    ActividadConVehiculos
    Actividad para entender un poco mas las clases y objetos

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pascual.Carros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("***** Menu ******");
+                Console.WriteLine("1. Ingresar vehiculos");
+                Console.WriteLine("2. Consultar vehiculo");
+                Console.WriteLine("3. Consultas todos");
+                Console.WriteLine("4. Salir");
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
